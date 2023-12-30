82b890a3c84cca02eebd2ddaebebcebf0b5177eb
Andy Vo <andyvo190998@gmail.com>
2023-12-30 16:05:27 +0100

    feat: handle route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,66 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Register';
+import Write from './pages/Write';
+import Single from './pages/Single';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Single />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/write',
+    element: <Write />,
+  },
+]);
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
