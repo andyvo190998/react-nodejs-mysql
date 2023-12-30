eded9bb5cc1c9a97027c3177ffacd98e4fe53c1b
Andy Vo <andyvo190998@gmail.com>
2023-12-30 19:36:31 +0100

    feat: create navbar and footer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Logo from '../img/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link to="/?cat=art" className="link">
+            <h6>ART</h6>
+          </Link>
+          <Link to="/?cat=art" className="link">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link to="/?cat=art" className="link">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link to="/?cat=art" className="link">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link to="/?cat=art" className="link">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link to="/?cat=art" className="link">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Andy</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
